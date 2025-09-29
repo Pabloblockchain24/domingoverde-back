12a29a49e6c2acc9e27934264d1c8c5d49543144
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-09-29 18:39:20 -0300

    review fixed

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -3,9 +3,11 @@ import express from "express";
 import {
   getReviews,
   createReview,
+  deleteReview
 } from "../controllers/review.controller.js";
 
 const router = express.Router();
 
 router.get("/", getReviews);
 router.post("/", createReview);
+router.delete("/:id", deleteReview) 
